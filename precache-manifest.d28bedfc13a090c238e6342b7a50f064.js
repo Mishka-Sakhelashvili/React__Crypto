057d9b11dc15c2e9a9c04fed161a60c632a2c9cc
Mikheil Sakhelashvili <mishka.sakhelashvili@gmail.com>
2021-03-12 17:01:53 +0400

    Updates

---
+++ precache-manifest.d28bedfc13a090c238e6342b7a50f064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc9249ed4358c41aa8f436ca8761a8f",
+    "url": "/React__Crypto/index.html"
+  },
+  {
+    "revision": "6dc978f9a03f340343a8",
+    "url": "/React__Crypto/static/css/main.2365a69a.chunk.css"
+  },
+  {
+    "revision": "38e7384c7dfc0992a550",
+    "url": "/React__Crypto/static/js/2.ba97f313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React__Crypto/static/js/2.ba97f313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc978f9a03f340343a8",
+    "url": "/React__Crypto/static/js/main.12dd9e2a.chunk.js"
+  },
+  {
+    "revision": "fa47094178e7b832615f",
+    "url": "/React__Crypto/static/js/runtime-main.77a399b6.js"
+  }
+]);
